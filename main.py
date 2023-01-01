150d8a31539c8aad086020b5b3c5514e106b9562
KMcAdams21 <mcadams.kendrick@gmail.com>
2022-12-31 22:48:19 -0600

    Simple data printing after API call

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+import requests
+
+
 # Program to give weather information when given information
-print("New text")
+
+# Creating API request
+url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&temperature_unit=fahrenheit&timezone=auto&daily=temperature_2m_max,temperature_2m_min,precipitation_sum&current_weather=true"
+req = requests.get(url)
+wetData = req.json()
+
+# Printing full result for testing means
+print(wetData)
+
+# Printing simple weather information
+print('The weather for the location at ' + str(wetData['latitude']) + ' ' + str(wetData['longitude']) + ':')
+print('The current temperature is ' + str(wetData['current_weather']['temperature']))
+print('The current wind speed is ' + str(wetData['current_weather']['windspeed']))
+print('The current wind direction is ' + str(wetData['current_weather']['winddirection']))
